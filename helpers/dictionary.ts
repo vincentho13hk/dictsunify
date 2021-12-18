c43e22f0557e3be73d4415ba19c1e078b475b22c
Vincent Ho <hoshukwan1@gmail.com>
2021-12-18 21:08:26 +0800

    display error

--- helpers/dictionary.ts
+++ helpers/dictionary.ts
@@ -35,211 +35,218 @@ export const createScreenshots = async (
   dictionary: string,
   word: string
 ): Promise<{ create: boolean; error?: string }> => {
-  if (dictionary === "macmillan") {
-    const browser = await Puppeteer.launch({
-      args: puppeteerLaunchArgs,
-    });
-    const page = await browser.newPage();
-    await page.goto("https://www.macmillandictionary.com/", {
-      waitUntil: "networkidle2",
-    });
-    // Enter the word and search
-    await page.focus("input[name=q]");
-    await page.keyboard.type(word);
-    await page.keyboard.press("Enter");
-    await page.waitForNavigation();
-    const content = await page.$("ol.senses");
-    if (content) {
-      const box = await content.boundingBox(); // this method returns an array of geometric parameters of the element in pixels.
-      if (box) {
-        const x = box["x"]; // coordinate x
-        const y = box["y"]; // coordinate y
-        const w = box["width"]; // area width
-        const h = box["height"]; // area height
-        const screenshotPath = `${screenshotsPath}/macmillan_${word}.png`;
-        await page.screenshot({
-          path: screenshotPath,
-          clip: { x: x, y: y, width: w, height: h },
-        }); // take screenshot of the required area in puppeteer
+  try {
+    if (dictionary === "macmillan") {
+      const browser = await Puppeteer.launch({
+        args: puppeteerLaunchArgs,
+      });
+      const page = await browser.newPage();
+      await page.goto("https://www.macmillandictionary.com/", {
+        waitUntil: "networkidle2",
+      });
+      // Enter the word and search
+      await page.focus("input[name=q]");
+      await page.keyboard.type(word);
+      await page.keyboard.press("Enter");
+      await page.waitForNavigation();
+      const content = await page.$("ol.senses");
+      if (content) {
+        const box = await content.boundingBox(); // this method returns an array of geometric parameters of the element in pixels.
+        if (box) {
+          const x = box["x"]; // coordinate x
+          const y = box["y"]; // coordinate y
+          const w = box["width"]; // area width
+          const h = box["height"]; // area height
+          const screenshotPath = `${screenshotsPath}/macmillan_${word}.png`;
+          await page.screenshot({
+            path: screenshotPath,
+            clip: { x: x, y: y, width: w, height: h },
+          }); // take screenshot of the required area in puppeteer
 
-        await browser.close(); // close browser
-        return {
-          create: true,
-        };
+          await browser.close(); // close browser
+          return {
+            create: true,
+          };
+        } else {
+          return {
+            create: false,
+            error: "box not found",
+          };
+        }
       } else {
         return {
           create: false,
-          error: "box not found",
+          error: "content not found",
         };
       }
-    } else {
-      return {
-        create: false,
-        error: "content not found",
-      };
-    }
-  } else if (dictionary === "wordreference_enzh") {
-    const browser = await Puppeteer.launch({
-      args: puppeteerLaunchArgs,
-    });
-    const page = await browser.newPage();
-    await page.goto("https://www.wordreference.com/enzh/", {
-      waitUntil: "networkidle2",
-    });
-    // Enter the word and search
-    await page.focus("input#si");
-    await page.keyboard.type(word);
-    await page.keyboard.press("Enter");
-    await page.waitForNavigation();
-    const content = await page.$("div#articleWRD > table.WRD");
-    if (content) {
-      const box = await content.boundingBox(); // this method returns an array of geometric parameters of the element in pixels.
-      if (box) {
-        const x = box["x"]; // coordinate x
-        const y = box["y"]; // coordinate y
-        const w = box["width"]; // area width
-        const h = box["height"]; // area height
+    } else if (dictionary === "wordreference_enzh") {
+      const browser = await Puppeteer.launch({
+        args: puppeteerLaunchArgs,
+      });
+      const page = await browser.newPage();
+      await page.goto("https://www.wordreference.com/enzh/", {
+        waitUntil: "networkidle2",
+      });
+      // Enter the word and search
+      await page.focus("input#si");
+      await page.keyboard.type(word);
+      await page.keyboard.press("Enter");
+      await page.waitForNavigation();
+      const content = await page.$("div#articleWRD > table.WRD");
+      if (content) {
+        const box = await content.boundingBox(); // this method returns an array of geometric parameters of the element in pixels.
+        if (box) {
+          const x = box["x"]; // coordinate x
+          const y = box["y"]; // coordinate y
+          const w = box["width"]; // area width
+          const h = box["height"]; // area height
+          const screenshotPath = `${screenshotsPath}/${dictionary}_${word}.png`;
+          await page.screenshot({
+            path: screenshotPath,
+            clip: { x: x, y: y, width: w, height: h },
+          }); // take screenshot of the required area in puppeteer
+
+          await browser.close(); // close browser
+          return {
+            create: true,
+          };
+        } else {
+          return {
+            create: false,
+            error: "box not found",
+          };
+        }
+      } else {
+        return {
+          create: false,
+          error: "content not found",
+        };
+      }
+    } else if (dictionary === "etymonline") {
+      const browser = await Puppeteer.launch({
+        args: puppeteerLaunchArgs,
+      });
+      const page = await browser.newPage();
+      await page.goto("https://www.etymonline.com/", {
+        waitUntil: "networkidle2",
+      });
+      // Enter the word and search
+      await page.focus("input[name=q]");
+      await page.keyboard.type(word);
+      await page.keyboard.press("Enter");
+      await page.waitForNavigation();
+      const searchList = await page.$("div[class^='searchList']");
+      if (searchList) {
+        const content = await searchList?.$x("following-sibling::*");
         const screenshotPath = `${screenshotsPath}/${dictionary}_${word}.png`;
-        await page.screenshot({
+        await content[0].screenshot({
           path: screenshotPath,
-          clip: { x: x, y: y, width: w, height: h },
         }); // take screenshot of the required area in puppeteer
-
-        await browser.close(); // close browser
         return {
           create: true,
         };
       } else {
         return {
           create: false,
-          error: "box not found",
+          error: "content not found",
+        };
+      }
+    } else if (dictionary === "wordreference_englishusage") {
+      const browser = await Puppeteer.launch({
+        args: puppeteerLaunchArgs,
+      });
+      const page = await browser.newPage();
+      await page.goto("https://www.wordreference.com/EnglishUsage/", {
+        waitUntil: "networkidle2",
+      });
+      // Enter the word and search
+      await page.focus("input#si");
+      await page.keyboard.type(word);
+      await page.keyboard.press("Enter");
+      await page.waitForNavigation();
+      const content = await page.$("div.entry");
+      if (content) {
+        const screenshotPath = `${screenshotsPath}/${dictionary}_${word}.png`;
+        await content.screenshot({
+          path: screenshotPath,
+        }); // take screenshot of the required area in puppeteer
+        return {
+          create: true,
+        };
+      } else {
+        return {
+          create: false,
+          error: "content not found",
+        };
+      }
+    } else if (dictionary === "wordreference_engdefinition") {
+      const { url, searchElement, element } =
+        dictionaryConfig.dictionaries.filter(
+          (dict) => dict.name === dictionary
+        )[0];
+      if (searchElement && element) {
+        return tryToCaptureScreenshot(
+          dictionary,
+          word,
+          url,
+          searchElement,
+          element
+        );
+      } else {
+        return {
+          create: false,
+          error: "dictionary not found",
+        };
+      }
+    } else if (dictionary === "vocabulary_com_definition") {
+      const { url, searchElement, element } =
+        dictionaryConfig.dictionaries.filter(
+          (dict) => dict.name === dictionary
+        )[0];
+      if (searchElement && element) {
+        return tryToCaptureScreenshot(
+          dictionary,
+          word,
+          url,
+          searchElement,
+          element
+        );
+      } else {
+        return {
+          create: false,
+          error: "dictionary not found",
+        };
+      }
+    } else if (dictionary === "vocabulary_com_usage") {
+      const { url, searchElement, element } =
+        dictionaryConfig.dictionaries.filter(
+          (dict) => dict.name === dictionary
+        )[0];
+      if (searchElement && element) {
+        return tryToCaptureScreenshot(
+          dictionary,
+          word,
+          url,
+          searchElement,
+          element,
+          2
+        );
+      } else {
+        return {
+          create: false,
+          error: "dictionary not found",
         };
       }
     } else {
       return {
         create: false,
-        error: "content not found",
-      };
-    }
-  } else if (dictionary === "etymonline") {
-    const browser = await Puppeteer.launch({
-      args: puppeteerLaunchArgs,
-    });
-    const page = await browser.newPage();
-    await page.goto("https://www.etymonline.com/", {
-      waitUntil: "networkidle2",
-    });
-    // Enter the word and search
-    await page.focus("input[name=q]");
-    await page.keyboard.type(word);
-    await page.keyboard.press("Enter");
-    await page.waitForNavigation();
-    const searchList = await page.$("div[class^='searchList']");
-    if (searchList) {
-      const content = await searchList?.$x("following-sibling::*");
-      const screenshotPath = `${screenshotsPath}/${dictionary}_${word}.png`;
-      await content[0].screenshot({
-        path: screenshotPath,
-      }); // take screenshot of the required area in puppeteer
-      return {
-        create: true,
-      };
-    } else {
-      return {
-        create: false,
-        error: "content not found",
-      };
-    }
-  } else if (dictionary === "wordreference_englishusage") {
-    const browser = await Puppeteer.launch({
-      args: puppeteerLaunchArgs,
-    });
-    const page = await browser.newPage();
-    await page.goto("https://www.wordreference.com/EnglishUsage/", {
-      waitUntil: "networkidle2",
-    });
-    // Enter the word and search
-    await page.focus("input#si");
-    await page.keyboard.type(word);
-    await page.keyboard.press("Enter");
-    await page.waitForNavigation();
-    const content = await page.$("div.entry");
-    if (content) {
-      const screenshotPath = `${screenshotsPath}/${dictionary}_${word}.png`;
-      await content.screenshot({
-        path: screenshotPath,
-      }); // take screenshot of the required area in puppeteer
-      return {
-        create: true,
-      };
-    } else {
-      return {
-        create: false,
-        error: "content not found",
-      };
-    }
-  } else if (dictionary === "wordreference_engdefinition") {
-    const { url, searchElement, element } =
-      dictionaryConfig.dictionaries.filter(
-        (dict) => dict.name === dictionary
-      )[0];
-    if (searchElement && element) {
-      return tryToCaptureScreenshot(
-        dictionary,
-        word,
-        url,
-        searchElement,
-        element
-      );
-    } else {
-      return {
-        create: false,
         error: "dictionary not found",
       };
     }
-  } else if (dictionary === "vocabulary_com_definition") {
-    const { url, searchElement, element } =
-      dictionaryConfig.dictionaries.filter(
-        (dict) => dict.name === dictionary
-      )[0];
-    if (searchElement && element) {
-      return tryToCaptureScreenshot(
-        dictionary,
-        word,
-        url,
-        searchElement,
-        element
-      );
-    } else {
-      return {
-        create: false,
-        error: "dictionary not found",
-      };
-    }
-  } else if (dictionary === "vocabulary_com_usage") {
-    const { url, searchElement, element } =
-      dictionaryConfig.dictionaries.filter(
-        (dict) => dict.name === dictionary
-      )[0];
-    if (searchElement && element) {
-      return tryToCaptureScreenshot(
-        dictionary,
-        word,
-        url,
-        searchElement,
-        element,
-        2
-      );
-    } else {
-      return {
-        create: false,
-        error: "dictionary not found",
-      };
-    }
-  } else {
+  } catch (err) {
     return {
       create: false,
-      error: "dictionary not found",
+      error: `error on capturing ${dictionary}`,
     };
   }
 };
@@ -252,48 +259,55 @@ const tryToCaptureScreenshot = async (
   element: string,
   delay?: number
 ): Promise<{ create: boolean; error?: string }> => {
-  const browser = await Puppeteer.launch({
-    args: puppeteerLaunchArgs,
-  });
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: "networkidle2",
-  });
-  // Enter the word and search
-  await page.focus(searchElement);
-  await page.keyboard.type(word);
-  await page.keyboard.press("Enter");
-  await page.waitForNavigation();
-  if (delay) {
-    await page.waitForTimeout(delay * 1000);
-  }
-  const content = await page.$(element);
-  if (content) {
-    const box = await content.boundingBox();
-    if (box) {
-      const x = box["x"]; // coordinate x
-      const y = box["y"]; // coordinate y
-      const w = box["width"]; // area width
-      const h = box["height"]; // area height
+  try {
+    const browser = await Puppeteer.launch({
+      args: puppeteerLaunchArgs,
+    });
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: "networkidle2",
+    });
+    // Enter the word and search
+    await page.focus(searchElement);
+    await page.keyboard.type(word);
+    await page.keyboard.press("Enter");
+    await page.waitForNavigation();
+    if (delay) {
+      await page.waitForTimeout(delay * 1000);
+    }
+    const content = await page.$(element);
+    if (content) {
+      const box = await content.boundingBox();
+      if (box) {
+        const x = box["x"]; // coordinate x
+        const y = box["y"]; // coordinate y
+        const w = box["width"]; // area width
+        const h = box["height"]; // area height
 
-      const screenshotPath = `${screenshotsPath}/${dictionary}_${word}.png`;
-      await content.screenshot({
-        path: screenshotPath,
-        clip: { x: x, y: y, width: w, height: h },
-      }); // take screenshot of the required area in puppeteer
-      return {
-        create: true,
-      };
+        const screenshotPath = `${screenshotsPath}/${dictionary}_${word}.png`;
+        await content.screenshot({
+          path: screenshotPath,
+          clip: { x: x, y: y, width: w, height: h },
+        }); // take screenshot of the required area in puppeteer
+        return {
+          create: true,
+        };
+      } else {
+        return {
+          create: false,
+          error: "box not found",
+        };
+      }
     } else {
       return {
         create: false,
-        error: "box not found",
+        error: "content not found",
       };
     }
-  } else {
+  } catch (err) {
     return {
       create: false,
-      error: "content not found",
+      error: String(err) || "Error on capturing screen",
     };
   }
 };
